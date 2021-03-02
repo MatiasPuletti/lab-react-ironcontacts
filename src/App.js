4c847cba42b031fdac4b1292ba19fe20c6b277aa
Matias Puletti <matiaspuletti@Matiass-MacBook-Pro.local>
2021-03-02 19:46:33 +0100

    Iteration 1 done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import contacts from './contacts.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+
+    const movieContacts = contacts.slice(0,5).map(value => {
+      return(
+        <tr>
+          <td><img src={value.pictureUrl}/></td>
+          <td>{value.name}</td>
+          <td>{Math.round(value.popularity * 100) / 100}</td>
+        </tr>
+      ) 
+    })
+
+    return (
+      <div className="App" >
+      <h1>IronContacts</h1>
+        <div className="Contacts">
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+          </tr>
+          {movieContacts}
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
